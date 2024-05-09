65720a2c89552a5ffdd6b9049e9f828cec936c4e
Mauricio Pereda Ruiz <ruizhydramauri09@gmail.com>
2024-05-08 22:53:53 -0500

    Introduccion a Javascript

---
+++ app.js
@@ -0,0 +1,42 @@
+// CARACTERISTICAS BASICAS DE JAVASCRIPT
+
+/*La función console.log() en JavaScript se utiliza para imprimir mensajes en la consola del navegador*/
+
+/*console.log('hola mundo');*/
+
+// VARIABLES 
+
+/*Las variables en javascript es como por ejemplo cajas que almacenan valores como numeros,texto o cualquier otro tipo de datos,existen 3 tipos de variables:*/
+
+//Var:Es la forma antigua de declarar variables en javascript
+
+var nombreVariable = valor;
+
+//let:Fue introducido en ECMAScript 6 (tambien conocido ES6) 'let' permite declarar variables con alcance de bloque.
+
+let nombreVariable = valor;
+
+//const:Tambien introducido en ECMAScript6 se utiliza para declarar variables cuyo valor no cambiara durante la ejecucion del programa.
+
+const Caja = valor;
+
+/*El color azul de la variable "Caja" se utiliza para resaltarlo como una palabra clave del lenguaje Javascript que se utilizara 
+  para declarar variables cuyo valores no cambiaran despues de ser inicializados.*/
+
+// Tipo de datos
+
+/*Los tipos de datos en JavaScript son las clasificaciones que se utilizan para definir los diferentes tipos de valores que pueden ser manipulados por el lenguaje.*/
+
+// Strings: Los string representan un texto.
+
+let Name = 'Mauri Pereda'; 
+
+// Numerico: Representa valores numericos,enteros o flotantes.
+
+let age = 30; // Numerico entero
+let number = 1.20; // float 
+
+// Boolean: Representan valores verdadero y falso.
+
+let Password = false;
+let PasswordCorrect = true;
---
+++ readme.md
@@ -0,0 +1,43 @@
+# JavaScript
+
+## ¿Que es JavaScript?
+
+JavaScript es un lenguaje de programación de alto nivel que se utiliza principalmente en el desarrollo web para agregar interactividad y dinamismo a las páginas HTML.
+
+![App Screenshot](image.png)
+
+## Variables en Javascript
+
+En JavaScript, hay varios tipos de variables que se pueden utilizar para almacenar diferentes tipos de datos. Aquí tienes una lista de los tipos de variables más comunes en JavaScript:
+
+- Var: Es la forma antigua de declarar variables en javascript
+- let: Fue introducido en ECMAScript 6 (tambien conocido ES6) 'let' permite declarar variables con alcance de bloque.
+- const: Tambien introducido en ECMAScript6 se utiliza para declarar variables cuyo valor no cambiara durante la ejecucion del programa.
+
+```Javascript
+var nombreVariable = valor;
+let nombreVariable = valor;
+const Caja = valor;
+```
+
+# Tipo de datos en Javascript 
+
+Los tipos de datos en JavaScript son las clasificaciones que se utilizan para definir los diferentes tipos de valores que pueden ser manipulados por el lenguaje.
+
+- Strings: Los string representan un texto.
+- Numerico: Representa valores numericos,enteros o flotantes.
+- Boolean: Representan valores verdadero y falso.
+
+```Javascript
+//STRING
+let Name = 'Mauri Pereda'; 
+
+// NUMERICO
+let age = 30; //ENTEROS
+let number = 1.20; //FLOAT
+
+// BOOLEAN
+let Password = false;
+let PasswordCorrect = true;
+```
+
